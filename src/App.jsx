e03e80e1b20999fc992333abf815abe34113f2e0
Nishant Dholakia <nishantdholakia2020@gmail.com>
2024-10-19 10:24:14 +0530

    done store and redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import {authService} from './appwrite/auth'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import {login,logout} from './store/authSlice'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-     <h1>Helooo Coderss</h1>
-    </>
-  )
+
+const [loading,setLoading] = useState(true);
+const dispatch = useDispatch();
+useEffect(()=>{
+  authService.getUser()
+  .then((userData)=>{
+    if(userData)
+    {
+      dispatch(login({userData}));
+    }
+    else{
+      dispatch(logout());
+    }
+  })
+  .finally(()=> setLoading(false));
+},[])
+
+
+
+ return !loading ? (
+  <div>
+    <Header />
+        TODO : 
+        {/* <Outlet /> */}
+    <Footer />
+  </div>
+ ) : null
 }
 
-export default App
+export default App;
